f1738dcf3c519167162e297582688b6126d4e8a6
mylo.xyloto <mintcarkeys@protonmail.com>
2024-12-12 01:48:54 -0600

    Initial Commit

---
+++ Metro 2033/Metro_2033App.swift
@@ -0,0 +1,17 @@
+//
+//  Metro_2033App.swift
+//  Metro 2033
+//
+//  Created by Pritesh Desai on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Metro_2033App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
